c09a3f7f6ac48e9717c496b07c112fc67af41374
bycait27 <caitlinba27@gmail.com>
2025-03-26 13:41:44 -0500

    Fixed comments not seeding

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { User, BlogPost } = require('../models');
+const { User, BlogPost, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const blogPostData = require('./blogPostData.json');
@@ -10,17 +10,63 @@ const seedDatabase = async () => {
     try {
         await sequelize.sync({ force: true });
 
+        // seed users
         const users = await User.bulkCreate(userData, {
-            individualHooks : true,
+            individualHooks: true,
             returning: true,
         });
 
-        for (const blogPost of blogPostData) {
-            await BlogPost.create({
-                ...blogPost,
-                user_id: users[Math.floor(Math.random() * users.length)].id
+        // seed blog posts
+        const blogPosts = await BlogPost.bulkCreate(blogPostData.map((post) => {
+            const randomUserIndex = Math.floor(Math.random() * users.length);
+            return {
+                ...post,
+                user_id: users[randomUserIndex].id,
+            };
+        }));
+
+        // seed comments
+        try {
+            const commentsToCreate = commentData.map((comment) => {
+                // find user by username
+                const user = users.find(u => u.username === comment.author);
+                if (!user) {
+                    console.log(`No user found with username ${comment.author}, using default user`);
+                }
+                
+                // get random blog post
+                const randomPostIndex = Math.floor(Math.random() * blogPosts.length);
+                
+                // Convert date string to proper date format - fixed to use correct year
+                const dateParts = comment.date_created.split('/');
+                const month = parseInt(dateParts[0]) - 1;   // Month (0-indexed)
+                const day = parseInt(dateParts[1]);        // Day
+                const year = parseInt(dateParts[2]);      // Year (already has 20)
+                
+                const formattedDate = new Date(2000 + year, month, day);
+                
+                return {
+                    content: comment.content,
+                    date_created: formattedDate,
+                    user_id: user ? user.id : users[0].id,
+                    blogpost_id: blogPosts[randomPostIndex].id
+                };
             });
+            
+            console.log('Comments to create:', commentsToCreate);
+            await Comment.bulkCreate(commentsToCreate);
+            console.log('Comments created successfully!');
+        } catch (commentError) {
+            console.error('Error creating comments:', commentError);
         }
+
+
+        // for (const blogPost of blogPostData) {
+        //     await BlogPost.create({
+        //         ...blogPost,
+        //         user_id: users[Math.floor(Math.random() * users.length)].id
+        //     });
+        // }
 
         // await Comment.bulkCreate(
         //     commentData.map((data) => ({
